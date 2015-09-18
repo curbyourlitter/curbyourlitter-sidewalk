eb4a66bdc775ae46e73788349fd03fbd26ab8fdf
Eric Brelsford <ebrelsford@gmail.com>
2015-09-18 00:20:43 -0400

    Mapbox tiles, sometimes

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -2,9 +2,8 @@ var config = {};
 
 config.apiBase = 'http://data.curbyourlitter.org';
 
-config.tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    minZoom: 11,
-    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
+L.mapbox.accessToken = 'pk.eyJ1IjoiY3VyYnlvdXJsaXR0ZXIiLCJhIjoiZjY1ZTdhODUxMTkwODMwZjc0MmQwOTM2OWRmMWJhNWQifQ.55lIFZ3my8bHNeLRq54Jig';
+
+config.tileLayer = L.mapbox.tileLayer('curbyourlitter.84a5e3d7');
 
 export default config;
