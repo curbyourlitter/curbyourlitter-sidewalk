c5f61b6aaf63d5e9bfa775d8347e93d06cac249e
Eric Brelsford <ebrelsford@gmail.com>
2015-10-14 11:14:27 -0400

    Litter sighting

--- app/scripts/ui/Request.jsx
+++ app/scripts/ui/Request.jsx
@@ -108,7 +108,7 @@ export var RequestListItem = hoverIntent(React.createClass({
                         </Col>
                         <Col xs={10}>
                             <div className="request-list-item-can-type">
-                                {this.props.can_type} bin request
+                                {this.props.can_type ? `${this.props.can_type} bin request` : 'litter sighting'}
                             </div>
                             <div className="request-list-item-date">
                                 {moment(this.props.date).format('h:mma MMMM Do YYYY')}
