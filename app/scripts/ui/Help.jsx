9da850c64d528fd075fc7834147ef30f11f84e82
Greg <greg@partnerandpartners.com>
2015-10-14 19:00:01 -0400

    Help Layout
    Help layout, styling and image updates

--- app/scripts/ui/Help.jsx
+++ app/scripts/ui/Help.jsx
@@ -51,6 +51,62 @@ var HelpItem = React.createClass({
     }
 });
 
+var HelpHeaderItem = React.createClass({
+    render: function () {
+        return (
+	        
+            <Col sm={6}>
+		        <h4 className="help-header"><img src={this.props.image} className="help-header-img" />{this.props.header}</h4>
+		       	{(() => {
+	                if (this.props.description) {
+	                    return <p className="type-description">{this.props.description}</p>;
+	                }
+            	})()}
+	        </Col>
+	        
+        );
+    }
+});
+
+var BlockRatingItem = React.createClass({
+    render: function () {
+        return (
+	       <div className="block-rating-descriptions">
+           <Col sm={2}>
+             <img src={this.props.image} className="img-responsive" />
+           </Col>
+           <Col sm={4}>
+             <h5 className="no-margin-top">{this.props.header}</h5>
+             {(() => {
+                if (this.props.body) {
+                    return <p>{this.props.body}</p>;
+                }
+            })()}
+            {this.props.children}
+          </Col>
+          </div>
+        );
+    }
+});
+
+var CommunityItem = React.createClass({
+    render: function () {
+        return (
+           <Col sm={3}>
+             <img src={this.props.image} className="img-responsive" />
+             <h5>{this.props.header}</h5>
+             {(() => {
+                if (this.props.body) {
+                    return <p>{this.props.body}</p>;
+                }
+            })()}
+            {this.props.children}
+          </Col>
+        );
+    }
+});
+
+
 var HelpCommunityInput = React.createClass({
     downloadUrl: function () {
         var sql = getRequestSql(null, null, getRequestColumnsDownload(config), config),
@@ -66,28 +122,40 @@ var HelpCommunityInput = React.createClass({
         return (
             <section className="help-type" id="community-input">
                 <h2>
-                    Community Input
+					Community Input
                     <Button className="help-download" href={this.downloadUrl()}>Download Data</Button>
                 </h2>
-                <p>
+                <h3>
                     In 2017, the Curb Your Litter project will install new litter and recycling bins throughout Greenpoint. We built this map to allow Greenpointers to help us decide the street corners that would best benefit from a new trash can. Users can choose from six different types of bins:
-                </p>
-                <section>
-                    <h3>Trash Bins</h3>
-                    <HelpItem header="Covered Litter Bin" image="images/bintypes/litter_standard.jpg" />
-                    <HelpItem header="BigBelly Solar Compactor Litter Bin" image="images/bintypes/litter_standard.jpg" />
-                </section>
-                <section>
-                    <h3>Recycling Bins</h3>
-                    <HelpItem header="Standard Bottle/Can Recycling Bin" image="images/bintypes/litter_standard.jpg" />
-                    <HelpItem header="Standard Paper Recycling Bin" image="images/bintypes/litter_standard.jpg" />
-                    <HelpItem header="BigBelly Solar Compactor for Bottle/Can Recycling" image="images/bintypes/litter_standard.jpg" />
-                    <HelpItem header="BigBelly Solar Compactor for Paper Recycling" image="images/bintypes/litter_standard.jpg" />
-                    <HelpItem header="BigBelly Solar Compactor for Paper Recycling" image="images/bintypes/litter_standard.jpg" />
-                </section>
-                <p>
-                    Each green basket on the map represents a vote for a new bin&mdash;click them on the map to read more about the request, or download a CSV file of all requests made to date.
-                </p>
+                </h3>
+                <Grid>
+                <Row>
+                <HelpHeaderItem header="Litter Requests" image="images/bintypes/litter_request_icon.png" description="Each green basket on the map represents a vote for a new bin—click them on the map to read more about the request, or download a CSV file of all requests made to date." />
+                </Row>
+				</Grid>
+                
+                    <section className="trash-bins">
+                    	<h4 className="subhead">Trash Bins</h4>
+		                <Grid>
+		                    <Row>
+		                    	<CommunityItem header="Covered Litter Bin" image="images/bintypes/litter_standard.jpg" body="A covered litter bin accepts all types of trash that someone walking on the street might need to throw away. An upside to this type of bin is that you can put anything in it, and the covered top prevents it from overflowing. Everything placed in this bin is sent to a landfill." />
+						   	 	<CommunityItem header="BigBelly Litter Bin" image="images/bintypes/litter_bigbelly.jpg" body="A BigBelly litter bin uses solar panels to compact trash, allowing for these bins to hold more trash than standard litter bins. Compactors can potentially reduce truck traffic they do not need be serviced as frequently as standard bins. These bins are more expensive to maintain, and everything that goes in is sent to a landfill." />
+						   	 
+						   </Row>
+						 </Grid>
+					</section>
+					<section className="recycling-bins">
+						<h4 className="subhead">Recycling Bins</h4>
+		                <Grid>
+		                    <Row>
+						   	 	<CommunityItem header="Standard Bottle & Can Recycling Bin" image="images/bintypes/recycling_standard_bottle.jpg" body="Bottle & Can recycling bins take only metal cans and plastic bottles. Cans and bottles thrown away in this type of bin are recycled. These bins encourage people to recycle and are easy to maintain." />
+						   	 	<CommunityItem header="Standard Paper Recycling Bin" image="images/bintypes/recycling_standard_paper.jpg" body="Paper recycling bins take only clean paper and magazines. All paper tossed in these bins are recycled. Standard reycling bins encourage people to recycle and are easy to maintain." />
+						   	 	<CommunityItem header="BigBelly Bottle & Can Recycling" image="images/bintypes/recycling_bigbelly_bottle.jpg" body="This type of bin accepts metal cans and plastic bottles only, and all cans and bottles thrown away in them are recycled.  A BigBelly recycler uses solar panels to power a machine that compacts trash within. Compactors can potentially reduce truck traffic they do not need be serviced as frequently as standard bins." />
+						   	 	<CommunityItem header="BigBelly Paper Recycling" image="images/bintypes/recycling_bigbelly_paper.jpg" body="This type of bin accepts clean paper and magazines only, and all paper thrown away in them is recycled. A BigBelly recycler uses solar panels to power a machine that compacts trash within. Compactors can potentially reduce truck traffic they do not need be serviced as frequently as standard bins." />
+						   	 </Row>
+						 </Grid>
+					</section>
+					
             </section>
         );
     }
@@ -96,11 +164,13 @@ var HelpCommunityInput = React.createClass({
 var HelpLitterSightings = React.createClass({
     render: function () {
         return (
-            <section className="help-type" id="litter-sightings">
-                <h2>Litter Sightings</h2>
-                <p>
-                    Photos of litter in the neighborhood illustrate the problem and help us identify areas that need the most attention. Submitting a photo of a particular corner, sidewalk or vacant lot strewn with trash is a way to do your part to curb our litter problem!  Click each litter sighting on the map to see the details and read more.
-                </p>
+            <section className="help-type">
+            <Grid>
+            	<Row>
+                <HelpHeaderItem header="Litter Sightings" image="images/bintypes/litter_sighting_icon.png" description="Photos of litter in the neighborhood illustrate the problem and help us identify areas that need the most attention. Submitting a photo of a particular corner, sidewalk or vacant lot strewn with trash is a way to do your part to curb our litter problem! Click each litter sighting on the map to see the details and read more." />
+                </Row>
+			</Grid>
+
             </section>
         );
     }
@@ -124,34 +194,59 @@ var Help311Data = React.createClass({
                     311 Data
                     <Button className="help-download" href={this.downloadUrl()}>Download Data</Button>
                 </h2>
-                <p>
-                    311 is New York City's main source of government information and non-emergency services. Residents can report littered streets and sidewalks by calling 311 or registering a complaint online. The map displays common complaints about litter in Greenpoint, including:
-                </p>
-                <HelpItem header="Sanitation Condition" image="images/bintypes/litter_standard.jpg">
-                    <div class="help-311-code">Code 15: Dirty Conditions / Corner Drop off</div>
-                    <p>Request for cleaning or the collection of items (e.g bags, bulk items) that have been dumped on a street or public area in front of a location or end of a street, etc.</p>
-                </HelpItem>
-                <HelpItem header="Overflowing Litter Baskets" image="images/bintypes/litter_standard.jpg">
-                    <div class="help-311-code">Code 6: Overflowing Litter Baskets</div>
-                    <p>Litter basket is overflowing (above the top) with litter falling out.</p>
-                </HelpItem>
-                <HelpItem header="Dirty Conditions" image="images/bintypes/litter_standard.jpg">
-                    <div class="help-311-code">Code E1: Improper Disposal</div>
-                    <p>When an individual leaves materials in front of the another person’s property and the complainant knows the residence of origin.</p>
-                    <div class="help-311-code">Code E2: Receptacle Violation</div>
-                    <p>Uncovered or overflowing receptacle or dumpster.</p>
-                    <div class="help-311-code">Code E3: Dirty Sidewalk</div>
-                    <p>Any property where the sidewalk area is not kept clean and contains litter/debris.</p>
-                    <div class="help-311-code">Code E3A: Dirty Area/Alleyway</div>
-                    <p>Unclean or untidy yards, areaways, alleys, and courts visible from the street.</p>
-                    <div class="help-311-code">Code E5: Loose Rubbish</div>
-                    <p>Newspapers, cardboard, and other debris that has been put out for collection but is not securely bundled or tied together.</p>
-                    <div class="help-311-code">Code E11: Litter Surveillance</div>
-                    <p>Chronic location where pedestrians fail to dispose of litter properly.</p>
-                    <div class="help-311-code">Code E12: Illegal Dumping Surveillance</div>
-                    <p>Chronic location where material is being dumped from a vehicle(s). Must include timeframes and days of the week when dumping tends to occurs.</p>
-                </HelpItem>
+                <h3>
+                    311 is New York Citys main source of government information and non-emergency services. Residents can report littered streets and sidewalks by calling 311 or registering a complaint online. The map displays common complaints about litter in Greenpoint, including:
+                </h3>
+                
+                <section className="311-data">
+		                <Grid>
+		                    <Row>
+						   	 	<Col sm={6}>
+						   	 		<h4><img src="images/bintypes/311_sanitation.png" className="help-header-img" />Sanitation Condition</h4>
+						   	 		<p><u>Code 15: Dirty Condition / Corner Drop off</u><br/>
+						   	 		Request for cleaning or the collection of items (e.g bags, bulk items) that have been dumped on a street or public area in front of a location or end of a street, etc.
+						   	 		</p>
+						   	 	</Col>
+						   	 	<Col sm={6}>
+						   	 		<h4><img src="images/bintypes/311_overflow.png" className="help-header-img" />Overflowing Litter Bin</h4>
+						   	 		<p><u>Code 6: Overflowing Litter Basket</u><br/>
+						   	 		Litter basket is overflowing (above the top) with litter falling out.
+						   	 		</p>
+						   	 	</Col>
+						   	 </Row>
+						   	 <Row>
+						   	 	<Col sm={6}>
+						   	 		<h4><img src="images/bintypes/311_dirty.png" className="help-header-img" />Dirty Condition</h4>
+						   	 		<p><u>Code E1: Improper Disposal</u><br/>
+										When an individual leaves materials in front of the another person’s property and the complainant knows the residence of origin.<br/>
+									
+										<u>Code E2: Receptacle Violation</u><br/>
+										Uncovered or overflowing receptacle or dumpster.<br/>
+										
+										<u>Code E3: Dirty Sidewalk</u><br/>
+										Any property where the sidewalk area is not kept clean and contains litter/debris.<br/>
+										
+										<u>Code E3A: Dirty Area/Alleyway</u><br/>
+										Unclean or untidy yards, areaways, alleys, and courts visible from the street.<br/>
+										
+										<u>Code E5: Loose Rubbish</u><br/>
+										Newspapers, cardboard, and other debris that has been put out for collection but is not securely bundled or tied together.<br/>
+										
+										<u>Code E11: Litter Surveillance</u><br/>
+										Chronic location where pedestrians fail to dispose of litter properly.<br/>
+										
+										<u>Code E12: Illegal Dumping Surveillance</u><br/>
+										Chronic location where material is being dumped from a vehicle(s). Must include timeframes and days of the week when dumping tends to occurs.
+						   	 		</p>
+						   	 	</Col>
+						   	 	
+						   	 </Row>
+						 </Grid>
+				</section>
+                
+                
             </section>
+
         );
     }
 });
@@ -169,19 +264,30 @@ var HelpBlockRatings = React.createClass({
 
     render: function () {
         return (
-            <section className="help-type" id="block-ratings">
-                <h2>
+            <section className="help-type block-ratings">
+
+               <h2>
                     Block Ratings
                     <Button className="help-download" href={this.downloadUrl()}>Download Data</Button>
                 </h2>
-                <p>
+                <h3>
                     We created a system for rating the cleanliness of our sidewalks on a 1-5 scale. Blocks are rated each year by the project team and volunteers at each of our Clean Up Days.
-                </p>
-                <HelpItem header="1: Clean" image="/images/bintypes/litter_standard.jpg" body="Little or no visible litter on the block surface sampled. [Recall that the survey for each block focuses on just one of the three “surfaces”—the sidewalks on both sides of the street and the street itself.]" />
-                <HelpItem header="2: Mostly Clean" image="/images/bintypes/litter_standard.jpg" body="A few discrete small items of litter." />
-                <HelpItem header="3: Visible Litter" image="/images/bintypes/litter_standard.jpg" body="More than just a few items of litter. Enough litter to be easily perceived and to induce an awareness that the street is not &ldquo;clean.&rdquo;" />
-                <HelpItem header="4: Lots of Litter" image="/images/bintypes/litter_standard.jpg" body="Fairly continuous items of litter. Enough litter to produce a distinct impression that the block surface has not been recently cleaned." />
-                <HelpItem header="5: Dirty" image="/images/bintypes/litter_standard.jpg" body="A major litter problem. Distinctly indicates that there are issues on the block such as those associated with homelessness, illegal dumping, drug use, abandonment, impaired property values, lack of full-time residents, small levels of pedestrian traffic associated with businesses." />
+                </h3>
+                <Grid>
+                <Row>
+	                <BlockRatingItem header="1: Clean" image="/images/bintypes/01_rating.png" body="Little or no visible litter on the block surface sampled. [Recall that the survey for each block focuses on just one of the three “surfaces”—the sidewalks on both sides of the street and the street itself.]" />
+	                <BlockRatingItem header="2: Mostly Clean" image="/images/bintypes/02_rating.png" body="A few discrete small items of litter." />
+                </Row>
+               
+                <Row>
+                	<BlockRatingItem header="3: Visible Litter" image="/images/bintypes/03_rating.png" body="More than just a few items of litter. Enough litter to be easily perceived and to induce an awareness that the street is not &ldquo;clean.&rdquo;" />
+                	<BlockRatingItem header="4: Lots of Litter" image="/images/bintypes/04_rating.png" body="Fairly continuous items of litter. Enough litter to produce a distinct impression that the block surface has not been recently cleaned." />
+                </Row>
+                
+                <Row>
+                	<BlockRatingItem header="5: Dirty" image="/images/bintypes/05_rating.png" body="A major litter problem. Distinctly indicates that there are issues on the block such as those associated with homelessness, illegal dumping, drug use, abandonment, impaired property values, lack of full-time residents, small levels of pedestrian traffic associated with businesses." />
+                </Row>
+                </Grid>
             </section>
         );
     }
@@ -190,13 +296,10 @@ var HelpBlockRatings = React.createClass({
 var HelpExistingCans = React.createClass({
     render: function () {
         return (
-            <section className="help-type" id="cans">
-                <h2>Existing Bins</h2>
-                <p>A dataset of all litter bins in the 11222 zip code is not publically available from the City of New York. We filled in this gap by surveying and mapping each bin in our neighborhood. There are currently two types of bins servicing Greenpoint:</p>
-                <HelpItem header="BigBelly Solar Compactors for Trash" image="images/bintypes/litter_standard.jpg" />
-                <HelpItem header="Standard Litter Bins" image="images/bintypes/litter_standard.jpg" />
-                <HelpItem header="Covered Litter Bins" image="images/bintypes/litter_standard.jpg" />
-            </section>
+            <section className="help-type">
+		        <h2>Existing Bins</h2>
+	            <h3>A dataset of all litter bins in the 11222 zip code is not publically available from the City of New York. We filled in this gap by surveying and mapping each bin in our neighborhood. There are currently two types of bins servicing Greenpoint</h3>
+            </section>	
         );
     }
 });
